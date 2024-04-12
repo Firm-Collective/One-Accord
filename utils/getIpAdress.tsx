20d14e0e554644e5b0badf4192b137b10425addf
Drew Morrison <77178810+Drew1128@users.noreply.github.com>
2024-04-11 20:52:35 -0500

    Add ipCountry field to User model

---
+++ utils/getIpAdress.tsx
@@ -0,0 +1,13 @@
+// get the country that the user is in from their ip address
+
+import axios from 'axios';
+
+export const getCountryFromIP = async (ip: string): Promise<string | null> => {
+  try {
+    const response = await axios.get(`https://ipapi.co/${ip}/json/`);
+    return response.data.country_name;
+  } catch (error) {
+    console.error('Error retrieving country from IP:', error);
+    return null;
+  }
+}
