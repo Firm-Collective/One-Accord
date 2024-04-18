bdedbe033c5670d4b496efa5d5d612df178fbd24
Drew Morrison <77178810+Drew1128@users.noreply.github.com>
2024-04-17 20:59:17 -0500

    Refactor onboarding page and backButton component

--- app/(onboarding)/page.tsx
+++ app/(onboarding)/page.tsx
@@ -1,71 +1,46 @@
 'use client';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useMutation, useQueryClient } from 'react-query';
 import Button from '@/components/button';
-import axios from 'axios';
 import Image from 'next/image';
 import BackButton from '@/components/backButton';
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react';
-
-
+import { useRouter } from 'next/navigation';
+import {  useState } from 'react';
 
 export default function GetStarted() {
-  const router = useRouter()
-  const [data, setData] = useState('heloo')
+  const router = useRouter();
+  const [data, setData] = useState('heloo');
 
   const handleGoBack = () => {
-    router.back
+    router.back;
   };
 
-  const handleAddData = () => {
-    setData('21345')
-  }
 
-  useEffect(() => {
-    console.log(data)
-  }, [data]);
 
   return (
     <main className='bg-white flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
-
         <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-          
           <div className='flex justify-center'>
-
-            <BackButton onClick={handleGoBack}/>
+            <BackButton onClick={handleGoBack} height={15} width={15} />
 
             <Image src='/one-accord.webp' alt='logo' width={293} height={48} />
 
-           {/* <Image src='/one-accord.webp' alt='logo' width={293} height={48} /> */}
-
+            {/* <Image src='/world-image.png' alt='logo' width={293} height={48} /> */}
           </div>
 
           <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
             Description Title
           </h2>
-          <button onClick={handleAddData}>Add data</button>
           <p className='mt-5 text-center text-sm text-gray-600 hover:text-gray-900'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Bibendum ut tristique et egestas quis.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ut labore et dolore magna
+            aliqua. Bibendum ut tristique et egestas quis.
           </p>
-          
         </div>
 
         <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-              <Button
-                variant='primary'
-                text='Get Started'
-                type='submit'
-              />
+          <Button variant='primary' text='Get Started' type='submit' />
 
-              <p className='mt-5 text-center text-sm text-gray-600 hover:text-gray-900'>
-                Skip to Live Stream
-              </p>
+          <p className='mt-5 text-center text-sm text-gray-600 hover:text-gray-900'>Skip to Live Stream</p>
         </div>
-
       </div>
     </main>
   );
--- components/backButton.tsx
+++ components/backButton.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import classNames from 'classnames';
 import Image from 'next/image';
 
 interface ImageButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick: () => void;
+  height: number;
+  width: number;
 }
 
-const BackButton: React.FC<ImageButtonProps> = ({ onClick }) => {
+const BackButton: React.FC<ImageButtonProps> = ({ onClick, height, width }) => {
 
 
   return (
     <button onClick={onClick}>
-        <Image src='/back-arrow-navigation.svg' alt='back' width={15} height={15}/>
+        <Image src='/back-arrow-navigation.svg' alt='back' width={width} height={height}/>
     </button>
   );
 };
