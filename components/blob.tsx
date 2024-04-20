e92b3b75ec6c7bda5f8f9992fe859c3b99ead5df
Josiah Parkhill <parkhilljosiah@gmail.com>
2024-04-20 17:09:16 -0500

    added Blob

---
+++ components/blob.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export default function Blob() {
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+  const colors = ['#DA7A84', '#F34F66', '#50c5e2', '#BBCFF3', '#F2DDCF', '#cc7b44', '#75A6AD', '#E2A9B0'];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    }, 5000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures the effect runs only once on component mount
+
+  return (
+    <>
+      <div
+        className={`absolute -z-[5] w-[420px] h-[456px] rounded-full opacity-55 blur-3xl animate-blob`}
+        style={{
+          backgroundColor: colors[currentColorIndex],
+          transition: 'background-color 2s ease-in-out',
+        }}
+      ></div>
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,34 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-poppins)'],
       },
+      animation: {
+        blob: 'blob 40s cubic-bezier(0.25, 0.1, 0.25, 1) infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%, 100%': {
+            transform: 'translate(0, 0) rotate(0deg)',
+          },
+          '20%': {
+            transform: 'translate(40vw, 20vh) rotate(90deg)',
+          },
+          '40%': {
+            transform: 'translate(30vw, 50vh) rotate(180deg)',
+          },
+          '60%': {
+            transform: 'translate(50vw, 40vh) rotate(270deg)',
+          },
+          '80%': {
+            transform: 'translate(20vw, 30vh) rotate(360deg)',
+          },
+          '90%': {
+            transform: 'translate(60vw, 60vh) rotate(450deg)',
+          },
+          '100%': {
+            transform: 'translate(0, 0) rotate(540deg)',
+          },
+        },
+      },
     },
   },
   plugins: [],
