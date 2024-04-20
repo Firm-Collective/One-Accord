650818d4c3ceb5bc222f9c7e5efcfe19cf7dc224
Andrew Ponce <me@drewponce.dev>
2024-04-20 11:14:33 -0700

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import '@fontsource/plus-jakarta-sans';
+import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
 import ReactQueryProvider from '@/utils/providers/ReactQueryProvider';
 import { ToastContainer } from 'react-toastify';
