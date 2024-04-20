e5673d02a862f4785944adbbfbcf626998f21a20
TheJnorio <jonatan.norio@gmail.com>
2024-04-20 15:44:10 -0500

    Logo change on welcome page
    I have changed the logo and some positioning on the welcome page.

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -27,10 +27,10 @@ const WelcomeScreen = () => {
       <div className='flex h-full flex-1 flex-col justify-center lg:px-8'>
         <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
           <div className='flex justify-center mt-auto'>
-            <Image src='/one-accord.webp' alt='logo' width={293} height={48} />
+            <Image src='/pink-logo.png' alt='logo' width={114} height={114} />
           </div>
 
-          <h2 className='mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900'>Welcome to</h2>
+          <h2 className='mt-20 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900'>Welcome to</h2>
           <h2 className='mt-2 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900 font-poppins'>
             <span className='font-bold'>one</span>
             <span className='font-normal'>accord</span>
@@ -40,7 +40,7 @@ const WelcomeScreen = () => {
           <br></br>
         </div>
 
-        <div className='mt-20 sm:mx-auto sm:w-full sm:max-w-sm grid gap-2'>
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm grid gap-2'>
           <Button variant='primary' text='Login' type='button' onClick={() => router.push('/auth/login')} />
 
           <Button variant='third' text='Sign Up' type='button' onClick={() => router.push('/auth/signup')} />
