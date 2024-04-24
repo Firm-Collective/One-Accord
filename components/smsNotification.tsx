394aec9ea8bbd61dddaa24f89f4a258d81d1bcf8
Ivana Soče Govorko <ivanasocegovorko@gmail.com>
2024-04-24 18:31:17 +0200

    start of smsNotification.tsx

--- components/smsNotification.tsx
+++ components/smsNotification.tsx
@@ -6,7 +6,7 @@ const client = require('twilio')(accountSid, authToken);
 client.messages
     .create({
         body: 'Ivanina aplikacija',
-        from: '+12563048389',
-        to: '+385994104110'
+        from: '',
+        to: ''
     })
     .then((message) => console.log(message.sid));
