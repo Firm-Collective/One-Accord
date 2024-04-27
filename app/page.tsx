dfbbfcad0694cd5d7735c84bf7635e4936f79c00
Charlyboy1993 <cjaramilloportilla@gmail.com>
2024-04-27 01:16:53 -0500

    Enable AuthenticationForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import AuthenticationForm from '@/components/authenticationForm';
-import CountdownTimer from '@/components/countDownTimer/countdownTimer';
 import { getUser } from '@/utils/supabase/auth';
 
 export default async function Home() {
@@ -8,15 +7,9 @@ export default async function Home() {
   return (
     <main className='bg-white flex min-h-screen flex-col items-center justify-center p-24'>
       {/* TODO:
-       This auth form is boilerplate that needs to be reworked
-       We need to think through the user experience for the authentication form.
-       1. We could have a separate login and signup forms
-       2. We could have a single form with a toggle between login and signup
-       3. We could follow the airbnb approach of having a single form that is loops through the database to see if 
-       the user exists and then logs them in or signs them up 
+      Implement authorization -> 
        */}
-      {/* <AuthenticationForm /> */}
-      <CountdownTimer />
+      <AuthenticationForm />
     </main>
   );
 }
