6533cab7dd77386e7a3fdb304e5db9f492f18b46
mgzs2013 <mariog.2822@gmail.com>
2024-05-14 22:39:19 -0700

    renaming functions and refactoring files so that client stays with client-side and server with server-side.

--- app/parent-feed/page.tsx
+++ app/parent-feed/page.tsx
@@ -3,22 +3,25 @@ import { supabaseServer } from '@/utils/supabase/server';
 import Header from '@/components/LiveFeed/Header';
 
 import InitUser from '@/utils/store/initUser';
-import AddMessages from '@/components/LiveFeed/AddMessages';
-import ListMessages from '@/components/LiveFeed/ListMessages';
+
 import FeedPosts from '@/components/LiveFeed/FeedPosts';
+
+
+
 export default async function page() {
   const supasbase = supabaseServer();
   const { data } = await (await supasbase).auth.getSession();
-
+ 
+  
   return (
     <>
       <div className='max-w-3xl mx-auto md:py-10 h-screen'>
         <div className='h-full border rounded-md'>
           <Header />
-          <FeedPosts  />
-          
+          <FeedPosts />
+
           <div className='p-5'>
-            <AddMessages />
+      
           </div>
         </div>
       </div>
--- components/LiveFeed/FeedPosts.tsx
+++ components/LiveFeed/FeedPosts.tsx
@@ -1,27 +1,34 @@
-import ListMessages from "./ListMessages";
-
-
-
-import { Suspense } from "react";
-import InitMessages from "@/utils/store/initMessages";
+import React, { Suspense } from "react";
+import { useQuery } from "react-query";
 import { supabaseServer } from "@/utils/supabase/server";
 import { LIMIT_MESSAGE } from "@/constant";
+import ListPosts from "./ListPosts";
+import AddPosts from "./AddPosts";
+import InitPosts from "@/utils/store/initPosts";
 
-export default async function ChatMessages() {
-	const supabase = await supabaseServer();
-
-	const { data } = await supabase
-		.from("Post")
-		.select("*,users(*)")
-		.range(0, LIMIT_MESSAGE)
-		.order("created_at", { ascending: false });
-
-	return (
-		<Suspense fallback={"loading.."}>
-			<ListMessages />
-			<InitMessages messages={data?.reverse() || []} />
-		</Suspense>
-	);
+async function fetchData() {
+  const supabase = await supabaseServer();
+  const { data } = await supabase
+    .from("Post")
+    .select("*,users(*)")
+    .range(0, LIMIT_MESSAGE)
+    .order("created_at", { ascending: false });
+  return data;
 }
+
+export default function FeedPosts() {
+  const { data: posts, refetch } = useQuery("posts", fetchData);
+
+  return (
+    <>
+      <Suspense fallback={"loading.."}>
+        <ListPosts />
+        <InitPosts posts={posts?.reverse() || []} />
+      </Suspense>
+      <AddPosts refetch={refetch} />
+    </>
+  );
+}
+
 
 
