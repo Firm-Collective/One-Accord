9ef3fb9fefd024fa6f20595318b423ca4a2adc63
Charlyboy1993 <cjaramilloportilla@gmail.com>
2024-05-15 21:21:54 -0500

    Implement Next.js router for authorization functionality

---
+++ app/(authenticated)/layout.tsx
@@ -0,0 +1,12 @@
+import { getUser } from '@/utils/supabase/auth';
+import { redirect } from 'next/navigation';
+
+export default async function DefaultLayout({ children }: { children: React.ReactNode }) {
+  const user = await getUser();
+
+  if (!user) {
+    redirect('login');
+  }
+
+  return <>{children}</>;
+}
