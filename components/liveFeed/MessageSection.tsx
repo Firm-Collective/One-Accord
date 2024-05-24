0b699cde27c895e5114ab722cb7d8f93c86bde2f
Charlyboy1993 <cjaramilloportilla@gmail.com>
2024-05-24 12:25:57 -0500

    Add filter for posts

--- components/liveFeed/MessageSection.tsx
+++ components/liveFeed/MessageSection.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
-import useLiveFeed from './hooks/useLiveFeed';
-import { type PostSchemaType } from './schemas';
+import { CretePostSchemaType, type PostSchemaType } from './schemas';
 
 type Props = {
   className: string;
@@ -10,51 +8,50 @@ type Props = {
   profilePictureClassName: string;
   rectangle?: string;
   image1?: string;
-  refetch: () => void;
   unsplashIfgrcqhznqg?: string;
-  posts: PostSchemaType[] | [];
+  posts: CretePostSchemaType[] | [];
 };
 
 const MessageSection: React.FC<Props> = ({
   className,
   ModeratorImage,
   profilePictureClassName,
-  refetch,
+  posts,
 }: Props): JSX.Element => {
-  const { queryPostInfo } = useLiveFeed();
-  const { data: posts, isLoading, error } = queryPostInfo;
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error loading posts.</div>;
-  }
+  const moderatorOrInfluencerPosts = posts.filter(
+    (post) =>
+      post.User.UserType.name === 'Moderator' || (post.User.UserType.name === 'Influencer' && post.is_visible === true),
+  );
+  const otherPosts = posts.filter(
+    (post) =>
+      post.User.UserType.name !== 'Moderator' && post.User.UserType.name !== 'Influencer' && post.is_visible === true,
+  );
 
   return (
     <>
       <div className={`inline-flex flex-col items-start gap-[5px] relative ${className} h-[150px] overflow-y-auto`}>
-        {/** render influencer/moderator */}
-        <div className='bg-[#d9d9d9] flex w-[369px] items-start gap-[10px] pl-[10px] pr-[46px] py-[4px] relative flex-[0_0_auto] rounded-[10px]'>
-          <Image className='relative w-[28px] h-[28px]' alt='Image' src={ModeratorImage} width={28} height={28} />
-          <p className="relative w-[300px] mt-[-1.00px] mr-[-25.00px] [font-family:'Poppins-SemiBold',Helvetica] font-normal text-[#171a1f] text-[12px] tracking-[0] leading-[15px]">
-            <span className='font-semibold'>Moderator</span>
-            <span className="[font-family:'Poppins-Regular',Helvetica]">&nbsp;</span>
-            <span className="[font-family:'Poppins-Light',Helvetica] font-light text-[11px]">
-              Today at 10:01 AM
-              <br />
-            </span>
-            <span className="[font-family:'Poppins-Regular',Helvetica]">
-              We are praying about Isaiah 9:6 &#34;For to us a child is born, to us a son is given, and the government
-              will be on his shoulders&#34;
-            </span>
-          </p>
-        </div>
-        {/* {posts?.map((post: Post) => ( <></> ))} */}
-        {posts?.map((post) => (
+        {/* Render moderator/influencer posts */}
+        {moderatorOrInfluencerPosts.map((post, idx) => (
           <div
-            key={post.id}
+            key={`${post.user_id}-${idx}`}
+            className='bg-[#d9d9d9] flex w-[369px] items-start gap-[10px] pl-[10px] pr-[46px] py-[4px] relative flex-[0_0_auto] rounded-[10px]'
+          >
+            <Image className='relative w-[28px] h-[28px]' alt='Image' src={ModeratorImage} width={28} height={28} />
+            <p className="relative w-[300px] mt-[-1.00px] mr-[-25.00px] [font-family:'Poppins-SemiBold',Helvetica] font-normal text-[#171a1f] text-[12px] tracking-[0] leading-[15px]">
+              <span className='font-semibold'>{post.User.UserType.name}</span>
+              <span className="[font-family:'Poppins-Regular',Helvetica]">&nbsp;</span>
+              <span className="[font-family:'Poppins-Light',Helvetica] font-light text-[11px]">
+                {new Date(post.created_at).toLocaleString()}
+                <br />
+              </span>
+              <span className="[font-family:'Poppins-Regular',Helvetica]">{post.content}</span>
+            </p>
+          </div>
+        ))}
+        {/* Render normal posts */}
+        {otherPosts?.map((post, idx) => (
+          <div
+            key={`${post.user_id}-${idx}`}
             className='bg-white flex w-[369px] items-start gap-[10px] pl-[10px] pr-[46px] py-[4px] relative flex-[0_0_auto] rounded-[10px]'
           >
             <div
