2bfb5819499c9b8637c4c4156370337c886a3d0c
Carlos Jaramillo <cjaramilloportilla@gmail.com>
2024-06-01 09:42:10 -0700

    Add footer icons and hide header

---
+++ components/landing/social-media/XIcon.tsx
@@ -0,0 +1,20 @@
+const XIcon = () => (
+  
+  <div className='group w-6 h-6'>
+    <svg
+      width='24'
+      height='22'
+      viewBox='0 0 24 22'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className='fill-current text-gray-500 group-hover:text-gray-400 transition-colors duration-300'
+    >
+      <path
+        d='M18.9014 0H22.5816L14.5415 9.31897L24 22H16.5941L10.7935 14.309L4.15631 22H0.473926L9.07356 12.0323L0 0H7.59394L12.8372 7.02985L18.9014 0ZM17.6098 19.7662H19.649L6.48589 2.11651H4.29759L17.6098 19.7662Z'
+        fill='currentColor'
+      />
+    </svg>
+  </div>
+);
+
+export default XIcon;
