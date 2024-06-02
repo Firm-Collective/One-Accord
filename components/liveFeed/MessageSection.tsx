a70c2abcab11e5dffd38f749d5e7a9355f7bd958
David Cao <sirdavidcao@gmail.com>
2024-06-02 02:34:20 -0600

    initial commit for scroll up feature

--- components/liveFeed/MessageSection.tsx
+++ components/liveFeed/MessageSection.tsx
@@ -1,157 +1,122 @@
-import { useState, useRef, useEffect } from 'react';
-import Image from 'next/image';
-import { CretePostSchemaType, type PostSchemaType } from './schemas';
-import useMessageSection from './hooks/useMessageSection';
-import axios from 'axios';
-import { Badge } from './ui/badge';
-import { InitialLoading, LoadingPinnedPosts, LoadingOtherPosts } from './ui/Loading';
-import useScroll from './hooks/useScroll';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { Database } from '@/utils/supabase/database.types';
+import { PostSchema, type PostSchemaType } from './schemas';
 
-type Pagination = {
-  from: number;
-  pageSize: number;
+export const postKeys = {
+  all: ['posts'] as const,
+  lists: () => [...postKeys.all, 'list'] as const,
+  list: (filters: string) => [...postKeys.lists(), { filters }] as const,
+  details: () => [...postKeys.all, 'detail'] as const,
+  detail: (id: string) => [...postKeys.details(), id] as const,
 };
 
-type UsePaginationReturn = {
-  pagination: Pagination;
-  setPagination: React.Dispatch<React.SetStateAction<Pagination>>;
-  incrementPagination: () => void;
-  getMaxPageSize: () => void;
-};
+export const postAPI = {
+  getPinnedPosts: async (params: { supaClient: SupabaseClient<Database>; from: number; pageSize: number }) => {
+    const { supaClient, from, pageSize } = params;
 
-type Props = {
-  className: string;
-  ModeratorImage: string;
-  rectangle?: string;
-  profilePictureClassName: string;
-  image1?: string;
-  unsplashIfgrcqhznqg?: string;
-  pinnedPosts: CretePostSchemaType[] | [];
-  registeredPosts: CretePostSchemaType[] | [];
-  paginationPinned: UsePaginationReturn;
-  paginationOther: UsePaginationReturn;
-  queryPinnedPosts: any;
-  queryOtherPosts: any;
-};
+    const query = supaClient
+      .from('Post')
+      .select(
+        `
+          *,
+          Activity(id, name),
+          Category(id, name),
+          Tag(id, name),
+          Sentiment(id, type),
+          Keywords(id, words, frequency),
+          Event(id, name),
+          MediaType(id, type),
+          User!inner(id, username, UserType:UserType!inner(id, name), Location:Location(id, city, country, latitude, longitude))
+        `,
+      )
+      .filter('is_visible', 'eq', true)
+      .in('User.UserType.name', ['Moderator', 'Influencer', 'Prophetic Organization'])
+      .range(from, pageSize)
+      .order('created_at', { ascending: false });
 
-export const fetchUserId = async () => {
-  try {
-    const response = await axios.get('/api/user/getUserId');
-    return response.data.userId ?? '';
-  } catch (error) {
-    console.error('Failed to fetch user ID:', error);
-    return '';
-  }
-};
+    const response = (await query).data;
 
-const MessageSection: React.FC<Props> = ({
-  className,
-  ModeratorImage,
-  profilePictureClassName,
-  pinnedPosts,
-  registeredPosts,
-  paginationPinned,
-  paginationOther,
-  queryPinnedPosts,
-  queryOtherPosts,
-}: Props): JSX.Element => {
-  const [userId, setUserId] = useState('');
+    const parsedSchema = PostSchema.safeParse(response);
 
-  const { moderatorOrInfluencerPosts, otherPosts, formatDate, queryUserTypeInfo } = useMessageSection({
-    pinnedPosts,
-    registeredPosts,
-    userId,
-  });
-  const containerModeratorOrInfluencerRef = useScroll(pinnedPosts, paginationPinned);
-  const containerOtherRef = useScroll(registeredPosts, paginationOther);
+    if (!parsedSchema.success) {
+      console.error('Error parsing schema:', parsedSchema.error);
+    }
 
-  useEffect(() => {
-    const fetchAndSetUserId = async () => {
-      const id = await fetchUserId();
-      setUserId(id);
+    return {
+      ...response,
+      data: parsedSchema.success ? parsedSchema.data : null,
     };
+  },
 
-    fetchAndSetUserId();
-  }, []);
+  getOtherPosts: async (params: { supaClient: SupabaseClient<Database>; from: number; pageSize: number }) => {
+    const { supaClient, from, pageSize } = params;
 
-  const showBadgeForUserType = (userType: string) => {
-    const validUserTypes = ['Moderator', 'Influencer', 'Prophetic Organization'];
-    return validUserTypes.includes(userType);
-  };
-  return (
-    <>
-      <div className={`inline-flex flex-col items-start relative ${className} h-[100%]`}>
-        {queryUserTypeInfo?.data?.name && showBadgeForUserType(queryUserTypeInfo.data.name) && (
-          <Badge userType={queryUserTypeInfo.data.name} className='self-end mb-2' />
-        )}
-        {/* Render moderator/influencer posts */}
-        {queryPinnedPosts.isLoading ? (
-         <LoadingPinnedPosts />
-        ) : (
-          moderatorOrInfluencerPosts?.length > 0 && (
-            <div
-              className={`flex flex-col items-start relative ${className} flex-grow-0 overflow-y-auto max-h-[30%] mb-1.5 w-[100%]`}
-              ref={containerModeratorOrInfluencerRef}
-            >
-              {moderatorOrInfluencerPosts?.map((post, idx) => (
-                <div
-                  key={`${post.user_id}-${idx}`}
-                  className='bg-[#d9d9d9] flex w-[369px] sm:w-full items-start gap-[10px] pl-[10px] pr-[46px] py-[4px] relative flex-[0_0_auto] rounded-[10px] mb-1'
-                >
-                  <Image
-                    className='relative w-[28px] h-[28px]'
-                    alt='Image'
-                    src={ModeratorImage}
-                    width={28}
-                    height={28}
-                  />
-                  <p className="relative w-[200px] sm:w-full mt-[-1.00px] mr-[-25.00px] [font-family:'Poppins-SemiBold',Helvetica] font-normal text-[#171a1f] text-[12px] tracking-[0] leading-[15px]">
-                    <span className='font-semibold'>{post.User.UserType.name}</span>
-                    <span className="[font-family:'Poppins-Regular',Helvetica]">&nbsp;</span>
-                    <span className="[font-family:'Poppins-Light',Helvetica] font-light text-[11px]">
-                      {formatDate(post.created_at)}
-                      <br />
-                    </span>
-                    <span className="[font-family:'Poppins-Regular',Helvetica]">{post.content}</span>
-                  </p>
-                </div>
-              ))}
-            </div>
-          )
-        )}
-        {/* Render normal posts */}
-        {queryOtherPosts.isLoading ? (
-          <LoadingOtherPosts />
-        ) : (
-          <div
-            className={`flex flex-col items-start gap-[5px] relative ${className} flex-grow-0 overflow-y-auto`}
-            ref={containerOtherRef}
-          >
-            {otherPosts?.map((post, idx) => (
-              <div
-                key={`${post.user_id}-${idx}`}
-                className='bg-white flex items-start gap-[10px] pl-[10px] pr-[46px] py-[4px] relative flex-[0_0_auto] rounded-[10px] w-[100%]'
-              >
-                <div
-                  className={`relative w-[28px] h-[28px] bg-[url(/profile-pic.png)] bg-[100%_100%] ${profilePictureClassName}`}
-                />
-                <p className="relative w-[200px] sm:w-full mt-[-1.00px] mr-[-25.00px] [font-family:'Poppins-SemiBold',Helvetica] font-normal text-[#171a1f] text-[12px] tracking-[0] leading-[15px]">
-                  <span className='font-semibold'>{post.User.username}</span>
-                  <span className="[font-family:'Poppins-Regular',Helvetica]">&nbsp;</span>
-                  <span className="[font-family:'Poppins-Light',Helvetica] font-light text-[11px]">
-                    <span>{post.User.Location?.city ? `(${post.User.Location.city}) ` : ''}</span>
-                    {formatDate(post.created_at)}
-                    <br />
-                  </span>
-                  <span className="[font-family:'Poppins-Regular',Helvetica]">{post.content}</span>
-                </p>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </>
-  );
+    const query = supaClient
+      .from('Post')
+      .select(
+        `
+          *,
+          Activity(id, name),
+          Category(id, name),
+          Tag(id, name),
+          Sentiment(id, type),
+          Keywords(id, words, frequency),
+          Event(id, name),
+          MediaType(id, type),
+          User!inner(id, username, UserType:UserType!inner(id, name), Location:Location(id, city, country, latitude, longitude))
+        `,
+      )
+      .filter('is_visible', 'eq', true)
+      .range(from, pageSize)
+      .in('User.UserType.name', ['Registered']) // add more types here?
+      .order('created_at', { ascending: false });
+
+    const response = (await query).data;
+
+    const parsedSchema = PostSchema.safeParse(response);
+
+    if (!parsedSchema.success) {
+      console.error('Error parsing schema:', parsedSchema.error);
+    }
+
+    return {
+      ...response,
+      data: parsedSchema.success ? parsedSchema.data : null,
+    };
+  },
+  getPostData: async (params: { supaClient: SupabaseClient<Database> }) => {
+    const { supaClient } = params;
+
+    const query = supaClient
+      .from('Post')
+      .select(
+        `
+      *,
+      Activity(id, name),
+      Category(id, name),
+      Tag(id, name),
+      Sentiment(id, type),
+      Keywords(id, words, frequency),
+      Event(id, name),
+      MediaType(id, type),
+      User(id, username, UserType:UserType(id, name), Location:Location(id, city, country, latitude, longitude))
+    `,
+      )
+      .filter('is_visible', 'eq', true)
+      .limit(100)
+      .order('created_at', { ascending: false });
+
+    const response = (await query).data;
+
+    const parsedSchema = PostSchema.safeParse(response);
+
+    if (!parsedSchema.success) {
+      console.error('Error parsing schema:', parsedSchema.error);
+    }
+
+    return {
+      ...response,
+      data: parsedSchema.success ? parsedSchema.data : null,
+    };
+  },
 };
-
-export default MessageSection;
