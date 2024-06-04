915600c5d806944ed8ece52d8e0f59b1c9bc3af0
David Cao <sirdavidcao@gmail.com>
2024-06-03 22:59:32 -0600

    rebased

--- components/liveFeed/hooks/useLiveFeed.ts
+++ components/liveFeed/hooks/useLiveFeed.ts
@@ -2,7 +2,6 @@ import { useQuery } from 'react-query';
 import { createClient } from '@/utils/supabase/client';
 import { postAPI, postKeys } from '../queries';
 import usePagination from './usePagination';
-<<<<<<< HEAD
 import { useState } from 'react';
 import { type PostSchemaType } from '../schemas';
 import useFetchPosts from './useFetchPosts';
@@ -55,103 +54,6 @@ const useLiveFeed = () => {
     paginationPinned,
     paginationOther,
     refetch,
-=======
-
-const useLiveFeed = () => {
-  const supaClient = createClient();
-  const pagination = usePagination();
-
-  // useQuery
-  const queryPostInfo = useQuery(
-    [...postKeys.lists(), pagination],
-    async () => {
-      try {
-        const postData = await postAPI.getPostData({
-          supaClient,
-          from: pagination.pagination.from,
-          pageSize: pagination.pagination.pageSize,
-        });
-        return postData?.data;
-      } catch (error) {
-        console.error('Error fetching post data:', error);
-        throw error;
-      }
-    },
-    {
-      onSuccess: (postData) => {
-        if (!postData) {
-          console.error('No data found on posts.');
-          return [];
-        }
-      },
-      onError: (error) => {
-        console.error('Error:', error);
-      },
-    },
-  );
-
-  const queryInfluencerOrModeratorPostInfo = useQuery(
-    [...postKeys.lists(), pagination],
-    async () => {
-      try {
-        const postData = await postAPI.getInfluencerOrModeratorPosts({
-          supaClient,
-          from: pagination.pagination.from,
-          pageSize: pagination.pagination.pageSize,
-        });
-        return postData?.data;
-      } catch (error) {
-        console.error('Error fetching post data:', error);
-        throw error;
-      }
-    },
-    {
-      onSuccess: (postData) => {
-        if (!postData) {
-          console.error('No data found on posts.');
-          return [];
-        }
-      },
-      onError: (error) => {
-        console.error('Error:', error);
-      },
-    },
-  );
-
-  const queryOtherPostInfo = useQuery(
-    [...postKeys.lists(), pagination],
-    async () => {
-      try {
-        const postData = await postAPI.getOtherPosts({
-          supaClient,
-          from: pagination.pagination.from,
-          pageSize: pagination.pagination.pageSize,
-        });
-        return postData?.data;
-      } catch (error) {
-        console.error('Error fetching post data:', error);
-        throw error;
-      }
-    },
-    {
-      onSuccess: (postData) => {
-        if (!postData) {
-          console.error('No data found on posts.');
-          return [];
-        }
-      },
-      onError: (error) => {
-        console.error('Error:', error);
-      },
-    },
-  );
-
-  return {
-    queryPostInfo,
-    queryInfluencerOrModeratorPostInfo,
-    queryOtherPostInfo,
-    pagination,
->>>>>>> 591c8e0 (initial commit for scroll up feature)
   };
 };
 
