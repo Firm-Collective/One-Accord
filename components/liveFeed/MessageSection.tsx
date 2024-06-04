05a200b51ddb208a1e31fbf33e538a6861408073
David Cao <sirdavidcao@gmail.com>
2024-06-04 00:41:44 -0600

    updated scrollup does not trigger loading skeleton

--- components/liveFeed/MessageSection.tsx
+++ components/liveFeed/MessageSection.tsx
@@ -82,7 +82,8 @@ const MessageSection: React.FC<Props> = ({
 
   return (
     <>
-      {queryPinnedPosts.isLoading || queryOtherPosts.isLoading ? (
+      {(queryPinnedPosts.isLoading && !paginationPinned.pagination.from) ||
+      (queryOtherPosts.isLoading && !paginationOther.pagination.from) ? (
         <div
           role='status'
           className='max-w-md p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700'
