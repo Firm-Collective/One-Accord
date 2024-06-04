05b536d32d6a52ae66df77f328eb8051c6e80e8e
Carlos Jaramillo <cjaramilloportilla@gmail.com>
2024-06-04 09:44:12 -0700

    Add loadings for live feed

--- components/liveFeed/MessageSection.tsx
+++ components/liveFeed/MessageSection.tsx
@@ -84,17 +84,18 @@ const MessageSection: React.FC<Props> = ({
   return (
     <>
       {queryPinnedPosts.isLoading || queryOtherPosts.isLoading ? (
-        <div role="status" className="max-w-md p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
-          {[...Array(5)].map((_, i) => (
-            <div key={i} className='flex items-center justify-between pt-4'>
-              <div>
-                <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5'></div>
-                <div className='w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
-              </div>
-            </div>
-          ))}
-          <span className='sr-only'>Loading...</span>
-        </div>
+        // <div role="status" className="max-w-md p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+        //   {[...Array(5)].map((_, i) => (
+        //     <div key={i} className='flex items-center justify-between pt-4'>
+        //       <div>
+        //         <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5'></div>
+        //         <div className='w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
+        //       </div>
+        //     </div>
+        //   ))}
+        //   <span className='sr-only'>Loading...</span>
+        // </div>
+        <Loading />
       ) : (
         <div className={`inline-flex flex-col items-start relative ${className} h-[100%]`}>
           {queryUserTypeInfo?.data?.name && showBadgeForUserType(queryUserTypeInfo.data.name) && (
