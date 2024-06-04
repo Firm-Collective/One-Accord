d6c5b6ec8c733c4d81a0799d05624f8881c86b16
David Cao <sirdavidcao@gmail.com>
2024-06-04 15:41:24 -0600

    made better loading divs

--- components/liveFeed/MessageSection.tsx
+++ components/liveFeed/MessageSection.tsx
@@ -64,9 +64,6 @@ const MessageSection: React.FC<Props> = ({
   });
   const containerModeratorOrInfluencerRef = useScroll(pinnedPosts, paginationPinned);
   const containerOtherRef = useScroll(registeredPosts, paginationOther);
-  const isInitialLoad =
-    (queryPinnedPosts.isLoading && !paginationPinned.pagination.from) ||
-    (queryOtherPosts.isLoading && !paginationOther.pagination.from);
 
   useEffect(() => {
     const fetchAndSetUserId = async () => {
@@ -83,69 +80,35 @@ const MessageSection: React.FC<Props> = ({
   };
   return (
     <>
-      {isInitialLoad ? (
-        <InitialLoading />
-      ) : (
-        <div className={`inline-flex flex-col items-start relative ${className} h-[100%]`}>
-          {queryUserTypeInfo?.data?.name && showBadgeForUserType(queryUserTypeInfo.data.name) && (
-            <Badge userType={queryUserTypeInfo.data.name} className='self-end mb-2' />
-          )}
-          {/* Render moderator/influencer posts */}
-          {queryPinnedPosts.isLoading ? (
-            <LoadingPinnedPosts />
-          ) : (
-            moderatorOrInfluencerPosts.length > 0 && (
-              <div
-                className={`flex flex-col items-start gap-[5px] relative ${className} flex-grow-0 overflow-y-auto max-h-[35%]`}
-                ref={containerModeratorOrInfluencerRef}
-              >
-                {moderatorOrInfluencerPosts?.map((post, idx) => (
-                  <div
-                    key={`${post.user_id}-${idx}`}
-                    className='bg-[#d9d9d9] flex w-[369px] items-start gap-[10px] pl-[10px] pr-[46px] py-[4px] relative flex-[0_0_auto] rounded-[10px]'
-                  >
-                    <Image
-                      className='relative w-[28px] h-[28px]'
-                      alt='Image'
-                      src={ModeratorImage}
-                      width={28}
-                      height={28}
-                    />
-                    <p className="relative w-[300px] mt-[-1.00px] mr-[-25.00px] [font-family:'Poppins-SemiBold',Helvetica] font-normal text-[#171a1f] text-[12px] tracking-[0] leading-[15px]">
-                      <span className='font-semibold'>{post.User.UserType.name}</span>
-                      <span className="[font-family:'Poppins-Regular',Helvetica]">&nbsp;</span>
-                      <span className="[font-family:'Poppins-Light',Helvetica] font-light text-[11px]">
-                        {formatDate(post.created_at)}
-                        <br />
-                      </span>
-                      <span className="[font-family:'Poppins-Regular',Helvetica]">{post.content}</span>
-                    </p>
-                  </div>
-                ))}
-              </div>
-            )
-          )}
-          {/* Render normal posts */}
-          {queryOtherPosts.isLoading ? (
-            <LoadingOtherPosts />
-          ) : (
+      <div className={`inline-flex flex-col items-start relative ${className} h-[100%]`}>
+        {queryUserTypeInfo?.data?.name && showBadgeForUserType(queryUserTypeInfo.data.name) && (
+          <Badge userType={queryUserTypeInfo.data.name} className='self-end mb-2' />
+        )}
+        {/* Render moderator/influencer posts */}
+        {queryPinnedPosts.isLoading ? (
+          <LoadingPinnedPosts />
+        ) : (
+          moderatorOrInfluencerPosts.length > 0 && (
             <div
-              className={`flex flex-col items-start gap-[5px] relative ${className} flex-grow-0 overflow-y-auto `}
-              ref={containerOtherRef}
+              className={`flex flex-col items-start gap-[5px] relative ${className} flex-grow-0 overflow-y-auto max-h-[35%]`}
+              ref={containerModeratorOrInfluencerRef}
             >
-              {otherPosts?.map((post, idx) => (
+              {moderatorOrInfluencerPosts?.map((post, idx) => (
                 <div
                   key={`${post.user_id}-${idx}`}
-                  className='bg-white flex w-[369px] items-start gap-[10px] pl-[10px] pr-[46px] py-[4px] relative flex-[0_0_auto] rounded-[10px]'
+                  className='bg-[#d9d9d9] flex w-[369px] items-start gap-[10px] pl-[10px] pr-[46px] py-[4px] relative flex-[0_0_auto] rounded-[10px]'
                 >
-                  <div
-                    className={`relative w-[28px] h-[28px] bg-[url(/profile-pic.png)] bg-[100%_100%] ${profilePictureClassName}`}
+                  <Image
+                    className='relative w-[28px] h-[28px]'
+                    alt='Image'
+                    src={ModeratorImage}
+                    width={28}
+                    height={28}
                   />
                   <p className="relative w-[300px] mt-[-1.00px] mr-[-25.00px] [font-family:'Poppins-SemiBold',Helvetica] font-normal text-[#171a1f] text-[12px] tracking-[0] leading-[15px]">
-                    <span className='font-semibold'>{post.User.username}</span>
+                    <span className='font-semibold'>{post.User.UserType.name}</span>
                     <span className="[font-family:'Poppins-Regular',Helvetica]">&nbsp;</span>
                     <span className="[font-family:'Poppins-Light',Helvetica] font-light text-[11px]">
-                      <span>{post.User.Location?.city ? `(${post.User.Location.city}) ` : ''}</span>
                       {formatDate(post.created_at)}
                       <br />
                     </span>
@@ -154,9 +117,39 @@ const MessageSection: React.FC<Props> = ({
                 </div>
               ))}
             </div>
-          )}
-        </div>
-      )}
+          )
+        )}
+        {/* Render normal posts */}
+        {queryOtherPosts.isLoading ? (
+          <LoadingOtherPosts />
+        ) : (
+          <div
+            className={`flex flex-col items-start gap-[5px] relative ${className} flex-grow-0 overflow-y-auto `}
+            ref={containerOtherRef}
+          >
+            {otherPosts?.map((post, idx) => (
+              <div
+                key={`${post.user_id}-${idx}`}
+                className='bg-white flex w-[369px] items-start gap-[10px] pl-[10px] pr-[46px] py-[4px] relative flex-[0_0_auto] rounded-[10px]'
+              >
+                <div
+                  className={`relative w-[28px] h-[28px] bg-[url(/profile-pic.png)] bg-[100%_100%] ${profilePictureClassName}`}
+                />
+                <p className="relative w-[300px] mt-[-1.00px] mr-[-25.00px] [font-family:'Poppins-SemiBold',Helvetica] font-normal text-[#171a1f] text-[12px] tracking-[0] leading-[15px]">
+                  <span className='font-semibold'>{post.User.username}</span>
+                  <span className="[font-family:'Poppins-Regular',Helvetica]">&nbsp;</span>
+                  <span className="[font-family:'Poppins-Light',Helvetica] font-light text-[11px]">
+                    <span>{post.User.Location?.city ? `(${post.User.Location.city}) ` : ''}</span>
+                    {formatDate(post.created_at)}
+                    <br />
+                  </span>
+                  <span className="[font-family:'Poppins-Regular',Helvetica]">{post.content}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </>
   );
 };
--- components/liveFeed/ui/Loading.tsx
+++ components/liveFeed/ui/Loading.tsx
@@ -19,13 +19,18 @@ export function InitialLoading() {
 
 export function LoadingPinnedPosts() {
   return (
-    <div role='status' className='max-w-sm animate-pulse'>
-      <div className='h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-96 mb-4'></div>
-      <div className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5'></div>
-      <div className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5'></div>
-      <div className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5'></div>
-      <div className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5'></div>
-      <div className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]'></div>
+    <div
+      role='status'
+      className='max-w-md space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700'
+    >
+      {[...Array(1)].map((_, i) => (
+        <div key={i} className='flex items-center justify-between pt-4'>
+          <div>
+            <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5'></div>
+            <div className=' w-80 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
+          </div>
+        </div>
+      ))}
       <span className='sr-only'>Loading...</span>
     </div>
   );
@@ -33,19 +38,18 @@ export function LoadingPinnedPosts() {
 
 export function LoadingOtherPosts() {
   return (
-    <div role='status' className='max-w-sm animate-pulse pt-4'>
-      <div className='h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-96 mb-4'></div>
-      <div className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5'></div>
-      <div className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5'></div>
-      {[...Array(10)].map((_, i) => {
-        return (
-          <div
-            key={i + 'load bar'}
-            className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5'
-          ></div>
-        );
-      })}
-      <div className='h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]'></div>
+    <div
+      role='status'
+      className='max-w-md space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700 mt-4'
+    >
+      {[...Array(4)].map((_, i) => (
+        <div key={i} className='flex items-center justify-between pt-4'>
+          <div>
+            <div className='h-1 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5'></div>
+            <div className=' w-80 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
+          </div>
+        </div>
+      ))}
       <span className='sr-only'>Loading...</span>
     </div>
   );
