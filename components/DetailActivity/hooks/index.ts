9163ee6fe7f79cdfc302fc5f78f75bcf1b04f5d5
Carlos Jaramillo <cjaramilloportilla@gmail.com>
2024-06-03 22:24:09 -0700

    Fix bug in activity.questions

--- components/DetailActivity/hooks/index.ts
+++ components/DetailActivity/hooks/index.ts
@@ -6,15 +6,15 @@ type Props = {
   };
 
 const useDetailActivityTimer = ({currentActivityIndex}: Props) => {
-    const activity = activities[currentActivityIndex]; 
+    const activity = activities[3]; 
     const [questionIndex, setQuestionIndex] = useState(0);
     const [shownQuestions, setShownQuestions] = useState<number[]>([]);
   
     const getRandomQuestionIndex = () => {
       let randomIndex;
       do {
-        randomIndex = Math.floor(Math.random() * activity.questions!?.length);
-      } while (shownQuestions.includes(randomIndex) && shownQuestions.length < activity.questions!?.length);
+        randomIndex = Math.floor(Math.random() * activity.questions!.length);
+      } while (shownQuestions.includes(randomIndex) && shownQuestions.length < activity.questions!.length);
   
       return randomIndex;
     };
@@ -22,7 +22,7 @@ const useDetailActivityTimer = ({currentActivityIndex}: Props) => {
     useEffect(() => {
       if (activity.name === 'Reflection' && activity.questions) {
         const interval = setInterval(() => {
-          if (shownQuestions.length === activity.questions!?.length) {
+          if (shownQuestions.length === activity.questions!.length) {
             setShownQuestions([]);
           } else {
             const newIndex = getRandomQuestionIndex();
