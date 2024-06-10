aa024cd8bd44ca31773f79284d98793c465974db
Carlos Jaramillo <cjaramilloportilla@gmail.com>
2024-06-09 21:26:51 -0700

    chore: Update loading component to use centralized spinner component

--- components/liveFeed/ui/Loading.tsx
+++ components/liveFeed/ui/Loading.tsx
@@ -1,3 +1,5 @@
+import Loading from "@/app/loading";
+
 export function InitialLoading() {
   return (
     <div
@@ -19,38 +21,44 @@ export function InitialLoading() {
 
 export function LoadingPinnedPosts() {
   return (
-    <div
-      role='status'
-      className='max-w-md space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700'
-    >
-      {[...Array(1)].map((_, i) => (
-        <div key={i} className='flex items-center justify-between pt-4'>
-          <div>
-            <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5'></div>
-            <div className=' w-80 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
-          </div>
-        </div>
-      ))}
-      <span className='sr-only'>Loading...</span>
+    // <div
+    //   role='status'
+    //   className='max-w-md space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700'
+    // >
+    //   {[...Array(1)].map((_, i) => (
+    //     <div key={i} className='flex items-center justify-between pt-4'>
+    //       <div>
+    //         <div className='h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5'></div>
+    //         <div className=' w-80 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
+    //       </div>
+    //     </div>
+    //   ))}
+    //   <span className='sr-only'>Loading...</span>
+    // </div>
+    <div className="flex items-center justify-center h-full">
+      <Loading />
     </div>
   );
 }
 
 export function LoadingOtherPosts() {
   return (
-    <div
-      role='status'
-      className='max-w-md space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700 mt-4'
-    >
-      {[...Array(4)].map((_, i) => (
-        <div key={i} className='flex items-center justify-between pt-4'>
-          <div>
-            <div className='h-1 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5'></div>
-            <div className=' w-80 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
-          </div>
-        </div>
-      ))}
-      <span className='sr-only'>Loading...</span>
+    // <div
+    //   role='status'
+    //   className='max-w-md space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700 mt-4'
+    // >
+    //   {[...Array(4)].map((_, i) => (
+    //     <div key={i} className='flex items-center justify-between pt-4'>
+    //       <div>
+    //         <div className='h-1 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5'></div>
+    //         <div className=' w-80 h-2 bg-gray-200 rounded-full dark:bg-gray-700'></div>
+    //       </div>
+    //     </div>
+    //   ))}
+    //   <span className='sr-only'>Loading...</span>
+    // </div>
+    <div className="flex items-center justify-center h-full">
+      <Loading />
     </div>
   );
 }
