de73223ea0cb80d3697a273f109187aa457dfa3e
Carlos Jaramillo <cjaramilloportilla@gmail.com>
2024-06-09 21:26:51 -0700

    chore: Update loading component to use centralized spinner component

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { Loading as Spinner } from '@/components/loading';
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center w-full h-full">
       <Spinner width={48} height={48} />
     </div>
   );
