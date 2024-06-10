a7bc12089b27512c9cc94bde9b3f89cd127dc2a3
David Cao <sirdavidcao@gmail.com>
2024-06-10 13:38:53 -0600

    fixed syncing issue

--- components/countDownTimer/hooks/useCurrentTime.ts
+++ components/countDownTimer/hooks/useCurrentTime.ts
@@ -9,8 +9,7 @@ type Props = {
   timeLeft: number;
 };
 
-const useCurrentTime = ({timeLeft}: Props) => {
-
+const useCurrentTime = ({ timeLeft }: Props) => {
   const [currentTime, setCurrentTime] = useState({ time: '', ampm: '', seconds: 0 });
   const initialTime: Time = {
     minutes: Math.floor(timeLeft / 60),
@@ -19,48 +18,45 @@ const useCurrentTime = ({timeLeft}: Props) => {
 
   const [currentTimeLeft, setCurrentTimeLeft] = useState<Time>(initialTime);
 
-    useEffect(() => {
-      const getTime = () => {
-        const date = new Date();
-        let hours = date.getHours();
-        const ampm = hours >= 12 ? 'PM' : 'AM';
-        hours = hours % 12 || 12;
-        const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
-        const formattedTime = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
-        setCurrentTime({ time: formattedTime, ampm, seconds });
-      };
-  
-      getTime();
-      const interval = setInterval(getTime, 1000); // 60000
-  
-      return () => clearInterval(interval);
-    }, []);
+  useEffect(() => {
+    const getTime = () => {
+      const date = new Date();
+      let hours = date.getHours();
+      const ampm = hours >= 12 ? 'PM' : 'AM';
+      hours = hours % 12 || 12;
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+      const formattedTime = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+      setCurrentTime({ time: formattedTime, ampm, seconds });
+    };
 
-    
-  
-    useEffect(() => {
-      const timer = setInterval(() => {
-        setCurrentTimeLeft((prevTimeLeft) => {
-          const newSeconds = prevTimeLeft.seconds === 0 ? 59 : prevTimeLeft.seconds - 1;
-          const newMinutes = prevTimeLeft.seconds === 0 ? prevTimeLeft.minutes - 1 : prevTimeLeft.minutes;
-  
-          if (newMinutes < 0) {
-            return { minutes: 15, seconds: 0 };
-          } else {
-            return { minutes: newMinutes, seconds: newSeconds };
-          }
-        });
-      }, 1000);
-  
-      return () => clearInterval(timer);
-    }, []);
-  
+    getTime();
+    const interval = setInterval(getTime, 1000); // 60000
 
-    return {
-        currentTime,
-        currentTimeLeft
-    }
-}
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTimeLeft((prevTimeLeft) => {
+        const newSeconds = prevTimeLeft.seconds === 0 ? 59 : prevTimeLeft.seconds - 1;
+        const newMinutes = prevTimeLeft.seconds === 0 ? prevTimeLeft.minutes - 1 : prevTimeLeft.minutes;
+
+        if (newMinutes < 0) {
+          return { minutes: 15, seconds: 0 };
+        } else {
+          return { minutes: newMinutes, seconds: newSeconds };
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return {
+    currentTime,
+    currentTimeLeft,
+  };
+};
 
 export default useCurrentTime;
