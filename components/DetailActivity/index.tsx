cce8d99a905ed2b29d8995e313751465093f7ebd
Carlos Jaramillo <cjaramilloportilla@gmail.com>
2024-06-10 22:51:13 -0700

    chore: Refactor DetailActivityTimer component and fix index out of bounds error

--- components/DetailActivity/index.tsx
+++ components/DetailActivity/index.tsx
@@ -7,44 +7,54 @@ type Props = {
 };
 
 const DetailActivityTimer: React.FC<Props> = ({ currentActivityIndex }) => {
-    const { activity, questionIndex } = useDetailActivityTimer({ currentActivityIndex })
+    const { activity, questionIndex } = useDetailActivityTimer({ currentActivityIndex });
     const setCurrentQuestion = useGlobalStore((state) => state.setCurrentQuestion);
 
     useEffect(() => {
         if (activity.questions && activity.indexQuestions) {
-          let adjustedQuestionIndex = questionIndex;
-          if (questionIndex >= 12) {
-            adjustedQuestionIndex = 0;
-          }
-          const currentQuestion = activity.indexQuestions[adjustedQuestionIndex][1];
-          setCurrentQuestion(adjustedQuestionIndex, currentQuestion);
+            let adjustedQuestionIndex = questionIndex;
+            if (questionIndex >= 12) {
+                adjustedQuestionIndex = 0;
+            }
+
+            if (
+                Array.isArray(activity.indexQuestions) &&
+                adjustedQuestionIndex >= 0 &&
+                adjustedQuestionIndex < activity.indexQuestions.length &&
+                Array.isArray(activity.indexQuestions[adjustedQuestionIndex]) &&
+                activity.indexQuestions[adjustedQuestionIndex].length > 1
+            ) {
+                const currentQuestion = activity.indexQuestions[adjustedQuestionIndex][1];
+                setCurrentQuestion(adjustedQuestionIndex, currentQuestion);
+            } else {
+                console.error('Invalid index or structure:', {
+                    adjustedQuestionIndex,
+                    indexQuestions: activity.indexQuestions,
+                });
+            }
         }
-      }, [activity, questionIndex, setCurrentQuestion]);
+    }, [activity, questionIndex, setCurrentQuestion]);
 
     return (
         <div className='w-full h-full flex flex-col items-center justify-center'>
-            {activity.name == 'Reflection' && (
+            {activity.name === 'Reflection' && (
                 <>
-                <h2 className="text-3xl font-extrabold text-gray-800 mb-6 text-center">{activity.name}</h2>
-                <p className="text-base font-medium text-gray-600 mb-6 text-center">
-                  Ask this question in prayer and join the conversation to write what you hear in the comments.
-                </p>
-              </>
-              
-              
+                    <h2 className="text-3xl font-extrabold text-gray-800 mb-6 text-center">{activity.name}</h2>
+                    <p className="text-base font-medium text-gray-600 mb-6 text-center">
+                        Ask this question in prayer and join the conversation to write what you hear in the comments.
+                    </p>
+                </>
             )}
             {activity.text && (
                 <div className='p-4 bg-gray-200 rounded shadow max-w-lg overflow-y-auto ml-10 mr-10 text-[12px] italic' style={{ height: '200px' }}>
-                    <p
-                        className='whitespace-pre-line'
-                        dangerouslySetInnerHTML={{ __html: activity.text }}
-                    ></p>
+                    <p className='whitespace-pre-line' dangerouslySetInnerHTML={{ __html: activity.text }}></p>
                 </div>
             )}
             {activity.videoUrl && (
                 <div className='relative w-full h-full bg-gray-200 rounded shadow overflow-hidden p-4' style={{ height: '100%' }}>
                     <iframe
-                        width="560" height="315"
+                        width="560"
+                        height="315"
                         className='absolute top-0 left-0 rounded-lg w-full h-full'
                         src={`${activity.videoUrl}`}
                         frameBorder='0'
@@ -56,7 +66,6 @@ const DetailActivityTimer: React.FC<Props> = ({ currentActivityIndex }) => {
             )}
             {activity.questions && activity.indexQuestions && questionIndex < activity.questions.length && questionIndex < activity.indexQuestions.length && (
                 <div className='p-4 bg-gray-200 rounded shadow ml-10 mr-10 text-[12px] italic max-w-lg'>
-                    {/* <p>{activity.questions[questionIndex]}</p> */}
                     <p>{activity.indexQuestions[questionIndex][1]}</p>
                 </div>
             )}
